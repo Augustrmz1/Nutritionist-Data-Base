be28b75525dcaf7c4c5d49d8b51b4c6a126dae16
Augustrmz1 <cramirez.e101@gmail.com>
2024-06-02 02:38:25 -0600

    Update Patleta.h

--- Patleta.h
+++ Patleta.h
@@ -1,29 +1,23 @@
-#ifndef PATLETA_H_INCLUDED
-#define PATLETA_H_INCLUDED
-#include "paciente.h"
+#ifndef PATLETA_H
+#define PATLETA_H
+#include "Paciente.h"
+#include <string>
 
-class Patleta : public Paciente {
+using namespace std;
+
+class Patleta : public Paciente
+{
 private:
     string deporte;
-    int nivel;
-
 
 public:
-    //constructor
-    Patleta(string nombre_paciente, char sexo_paciente, int edad_paciente, int altura_paciente, double peso_paciente, int frecuenciaentreno_paciente, int objetivo_paciente, int grasa_percent_paciente, string deporte_paciente, int nivel_paciente);
+    Patleta(string, char, int, int, double, string);
 
-    //setters
-    void setDeporte(string deporte_paciente);
-    void setNivel(int nivel_paciente);
-
-    //getters
+    void setDeporte(string);
     string getDeporte();
-    int getNivel();
 
-    //specific athlete methods
-    void infoAtleta();
-    string str_nivelDeporte();
-    
+    int calcularCalorias() const override;
+    void datosPaciente() const override;
 };
 
 #endif
