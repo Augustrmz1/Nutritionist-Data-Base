2eb6afc1dc6a7cabfbb3553499f8130e86a8db89
Augustrmz1 <cramirez.e101@gmail.com>
2024-06-02 02:39:37 -0600

    Update Pinfantil.cpp

--- Pinfantil.cpp
+++ Pinfantil.cpp
@@ -1,23 +1,83 @@
 #include "Pinfantil.h"
+#include <iostream>
 #include <string>
-Pinfantil::Pinfantil(string nombre_paciente, char sexo_paciente, int edad_paciente, int altura_paciente,double peso_paciente, int frecuenciaentreno_paciente, int objetivo_paciente, int grasa_percent_paciente) : Paciente(nombre_paciente, sexo_paciente, edad_paciente, altura_paciente, peso_paciente,frecuenciaentreno_paciente, objetivo_paciente, grasa_percent_paciente) {}
 
-void Pinfantil::infoPinfantil() {
-    datos_paciente();
-    cout << "Etapa de desarrollo: " << etapaDesarrollo() << endl;
+using namespace std;
+// constructor
+Pinfantil::Pinfantil(string _nombre, char _sexo, int _edad, int _altura, double _peso, string _nombreTutor) : Paciente(_nombre, _sexo, _edad, _altura, _peso)
+{
+    nombreTutor = _nombreTutor;
 }
 
-string Pinfantil::etapaDesarrollo() {
-  if (edad >= 0 && edad <= 2) {
-    return "bebe";
-  } else if (edad >2 && edad <= 5) {
-    return "Primera infancia";
-  } else if (edad >5 && edad <= 12) {
-    return "Infancia";
-  } else if (edad >12 && edad <=17) {
-    return "Adolescencia";
-  } else{
-    return "Este paciente no pertenece a la categoria infantil.";
-  }
-  
+// metodos get-set de paciente infantil
+void Pinfantil::setNombreTutor(string _nombretutor)
+{
+    nombreTutor = _nombretutor;
+}
+
+string Pinfantil::getNombreTutor()
+{
+    return nombreTutor;
+}
+
+// metodo especifico de la clase infantil
+string Pinfantil::etapaDesarrollo()
+{
+    if (edad >= 0 && edad <= 2)
+    {
+        return "Bebe";
+    }
+    else if (edad > 2 && edad <= 5)
+    {
+        return "Primera infancia";
+    }
+    else if (edad > 5 && edad < 12)
+    {
+        return "Infancia";
+    }
+    else if (edad > 12 && edad > 17)
+    {
+        return "Adolescencia";
+    }
+    else
+    {
+        return "Este paciente no pertenece a la categoria infantil.";
+    }
+}
+
+int Pinfantil::calcularCalorias() const
+{
+    if (edad >= 0 && edad <= 2)
+    {
+        return 1000;
+    }
+    else if (edad > 2 && edad <= 10)
+    {
+        return 88.5 + (10 * peso) + (2 * altura) - (5 * edad) + 20;
+    }
+    else if (sexo == 'H' && edad > 10)
+    {
+        return 88.362 + (13.397 * peso) + (4.799 * altura);
+    }
+    else if (sexo == 'M' && edad > 10)
+    {
+        return 447.593 + (9.247 * peso) + (3.098 * altura);
+    }
+    else
+    {
+        return 0;
+        cout << "sexo no valido, no se continuara con el calculo" << endl;
+    }
+}
+
+void Pinfantil::datosPaciente() const
+{
+    cout << "Datos del paciente: " << endl;
+    cout << "Nombre: " << nombre << endl;
+    cout << "Nombre del tutor: " << nombreTutor << endl;
+    cout << "Sexo: " << sexo << endl;
+    cout << "Edad: " << edad << " anio(s)" << endl;
+    cout << "Peso: " << peso << " kg" << endl;
+    cout << "Altura: " << altura << " cm" << endl;
+    cout << "Consumo calorico recomendado: " << calcularCalorias() << " kcal" << endl;
 }
